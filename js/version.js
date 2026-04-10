ee0fb98af50882f3123c0711d88cfde7b7f81169
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 16:42:12 -0700

    build: refresh prod version metadata

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.0';window.__GIT_HASH__='31c94a';window.__BUILD_DATE__='2026-04-10';
+window.__VERSION__='0.3.0';window.__GIT_HASH__='fe449d';window.__BUILD_DATE__='2026-04-10';
