beb3af39aa04b82822ea82fb4166cd43c8fb5969
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 12:07:11 -0700

    build: bump version after attack dog roster

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.27';window.__GIT_HASH__='841d28';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.28';window.__GIT_HASH__='d90f40';window.__BUILD_DATE__='2026-04-11';
