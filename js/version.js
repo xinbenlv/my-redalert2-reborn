ed41573f4a7d3dc394518d4982a2ce3cfb80acd9
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 10:02:12 -0700

    build: bump version after IFV escort vehicle

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.20';window.__GIT_HASH__='a5db3a';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.21';window.__GIT_HASH__='4c4684';window.__BUILD_DATE__='2026-04-11';
