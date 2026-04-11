7c00ab0a0131cd7c42912fb2e99af478af392343
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 10:40:03 -0700

    build: bump version after APC transport AI

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.22';window.__GIT_HASH__='a2e696';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.23';window.__GIT_HASH__='2de52a';window.__BUILD_DATE__='2026-04-11';
