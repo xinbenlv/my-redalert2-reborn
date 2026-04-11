88cf0d09ff76437e78b4514d80659f2f013b709e
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 12:56:14 -0700

    build: bump version after patriot battery

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.30';window.__GIT_HASH__='03f70b';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.31';window.__GIT_HASH__='5c3eba';window.__BUILD_DATE__='2026-04-11';
