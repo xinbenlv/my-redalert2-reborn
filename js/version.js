f6012eb090ce738b4e0816619f4df08a4c5683c7
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 19:57:35 -0700

    build: bump version after production controls

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.0';window.__GIT_HASH__='fe449d';window.__BUILD_DATE__='2026-04-10';
+window.__VERSION__='0.3.1';window.__GIT_HASH__='d2cea3';window.__BUILD_DATE__='2026-04-11';
