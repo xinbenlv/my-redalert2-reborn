ccde04a08d365b2f34a650ada4608af833ea42c8
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 14:45:05 -0700

    build: bump version after multi-ai skirmish support

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.37';window.__GIT_HASH__='13a863';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.38';window.__GIT_HASH__='5be773';window.__BUILD_DATE__='2026-04-11';
