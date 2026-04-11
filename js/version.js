90408a1bc46340d5d1942b57f14a0d5d752dd9d1
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 23:58:47 -0700

    build: bump version after base defenses

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.6';window.__GIT_HASH__='6d9362';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.7';window.__GIT_HASH__='29ce1d';window.__BUILD_DATE__='2026-04-11';
