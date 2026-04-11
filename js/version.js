c4bdb0597b7895d0a257fcb31423cc3e60103253
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 11:49:47 -0700

    build: bump version after service depot repairs

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.26';window.__GIT_HASH__='61ea36';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.27';window.__GIT_HASH__='841d28';window.__BUILD_DATE__='2026-04-11';
