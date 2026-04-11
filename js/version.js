39adf24370175575970c235714a8d5a4c5003a61
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 11:10:14 -0700

    build: bump version after AI air awareness

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.24';window.__GIT_HASH__='8e98c5';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.25';window.__GIT_HASH__='a5182e';window.__BUILD_DATE__='2026-04-11';
