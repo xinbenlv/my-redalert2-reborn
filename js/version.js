9f09b9a31811b48c66436939f63ba5ea9f3df683
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 08:26:51 -0700

    build: bump version after air units

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.16';window.__GIT_HASH__='9b77a5';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.17';window.__GIT_HASH__='8b39c5';window.__BUILD_DATE__='2026-04-11';
