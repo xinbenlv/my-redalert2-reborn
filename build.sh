d0e181833702cadf51550437ef861fde080c2e37
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 13:22:04 -0700

    build: bump version after prerequisite visuals

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ else
     exit 1
 fi
 
-VERSION="0.3.32"
+VERSION="0.3.33"
 DATE=$(date -u +"%Y-%m-%d")
 echo "window.__VERSION__='${VERSION}';window.__GIT_HASH__='${HASH}';window.__BUILD_DATE__='${DATE}';" > js/version.js
 echo "Built version ${VERSION} (${HASH}) on ${DATE}"
--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.32';window.__GIT_HASH__='71f96c';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.33';window.__GIT_HASH__='cabf11';window.__BUILD_DATE__='2026-04-11';
