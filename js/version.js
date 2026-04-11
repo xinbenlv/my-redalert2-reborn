cfd6a41aad82d0ab018c8985dc8021e01efb58bd
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 20:03:20 -0700

    build: bump version after cancellation refunds

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.1';window.__GIT_HASH__='d2cea3';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.2';window.__GIT_HASH__='d7d5ea';window.__BUILD_DATE__='2026-04-11';
