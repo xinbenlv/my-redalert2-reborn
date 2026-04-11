23b0e577c32ddfb08f9ff3734f119fb9ce9ef8a6
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 15:22:16 -0700

    build: bump version after force-move command

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.39';window.__GIT_HASH__='4ca2f3';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.40';window.__GIT_HASH__='7f2d9c';window.__BUILD_DATE__='2026-04-11';
