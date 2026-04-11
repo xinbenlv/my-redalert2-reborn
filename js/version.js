fd1905318b8bd2089f7d6eb17a75db859d56bb67
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 11:33:59 -0700

    build: bump version after battle bunker garrisons

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.25';window.__GIT_HASH__='a5182e';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.26';window.__GIT_HASH__='61ea36';window.__BUILD_DATE__='2026-04-11';
