3a1c579b85f328ce3ee4eb6dfaead50518fcbd88
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 09:08:24 -0700

    build: bump version after harrier rearm loop

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.17';window.__GIT_HASH__='8b39c5';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.18';window.__GIT_HASH__='adc6ec';window.__BUILD_DATE__='2026-04-11';
