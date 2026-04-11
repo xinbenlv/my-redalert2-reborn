f11572e94d0d6d982f2bab6ceb0f718428b40331
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 00:39:51 -0700

    build: bump version after AI harvester pressure

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.7';window.__GIT_HASH__='29ce1d';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.8';window.__GIT_HASH__='f84643';window.__BUILD_DATE__='2026-04-11';
