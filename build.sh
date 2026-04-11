42884e0051f823f336bdc01a596f6b0ce8acd7fa
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 05:41:00 -0700

    build: bump version after veterancy system

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ else
     exit 1
 fi
 
-VERSION="0.3.13"
+VERSION="0.3.14"
 DATE=$(date -u +"%Y-%m-%d")
 echo "window.__VERSION__='${VERSION}';window.__GIT_HASH__='${HASH}';window.__BUILD_DATE__='${DATE}';" > js/version.js
 echo "Built version ${VERSION} (${HASH}) on ${DATE}"
--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.13';window.__GIT_HASH__='a0c42d';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.14';window.__GIT_HASH__='9a6b3e';window.__BUILD_DATE__='2026-04-11';
