7fe5846c122b12e9a7173dc7e44d1d35762fa35e
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 20:39:21 -0700

    build: bump version after rally overlays

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.2';window.__GIT_HASH__='d7d5ea';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.3';window.__GIT_HASH__='1b3ef7';window.__BUILD_DATE__='2026-04-11';
