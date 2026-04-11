3a8599858bb18a55722fba56ee3bb6dcec054095
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 09:48:22 -0700

    build: bump version after advanced power plant

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.19';window.__GIT_HASH__='2f4439';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.20';window.__GIT_HASH__='a5db3a';window.__BUILD_DATE__='2026-04-11';
