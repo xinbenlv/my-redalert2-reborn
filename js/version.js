7e66130a154a4de119b71421161d92109953afa3
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 10:55:03 -0700

    build: bump version after air feedback

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.23';window.__GIT_HASH__='2de52a';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.24';window.__GIT_HASH__='8e98c5';window.__BUILD_DATE__='2026-04-11';
