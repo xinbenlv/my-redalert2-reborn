f6cf0d0e627ff9110f97129543b85e4284b2549f
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 02:55:51 -0700

    build: bump version after battle lab tier

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.10';window.__GIT_HASH__='83ba67';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.11';window.__GIT_HASH__='be8401';window.__BUILD_DATE__='2026-04-11';
