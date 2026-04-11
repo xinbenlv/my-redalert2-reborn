49ea39bb1f35c0b9b90eb0b83489b6e68a55b268
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 16:54:51 -0700

    build: bump version after guard command

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.44';window.__GIT_HASH__='5ca5d8';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.45';window.__GIT_HASH__='026b72';window.__BUILD_DATE__='2026-04-11';
