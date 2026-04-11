7f1dee3f86f0dbed8e17611f96bbdd849d3778bc
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 22:34:46 -0700

    build: bump version after engineer capture

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.5';window.__GIT_HASH__='d5e393';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.6';window.__GIT_HASH__='6d9362';window.__BUILD_DATE__='2026-04-11';
