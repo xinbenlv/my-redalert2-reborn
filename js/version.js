a20efb5c4d95819a675269788c4e67e69dc1950b
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 13:39:43 -0700

    build: bump version after patrol command

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.33';window.__GIT_HASH__='cabf11';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.34';window.__GIT_HASH__='f91a4b';window.__BUILD_DATE__='2026-04-11';
