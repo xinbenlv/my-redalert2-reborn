76f4ccc90bc2aeec851fbae710bad5383ef1f10c
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 03:38:32 -0700

    build: bump version after endgame summary

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.11';window.__GIT_HASH__='be8401';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.12';window.__GIT_HASH__='fbcce6';window.__BUILD_DATE__='2026-04-11';
