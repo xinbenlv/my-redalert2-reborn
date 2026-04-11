465332547fb1c23cf6b1fd0d01624f526a1d9a6e
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 09:33:34 -0700

    build: bump version after APC unit

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.18';window.__GIT_HASH__='adc6ec';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.19';window.__GIT_HASH__='2f4439';window.__BUILD_DATE__='2026-04-11';
