78c9cae41a1d7aba665aa235f44879632ec1e0e6
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 04:59:43 -0700

    build: bump version after twin rivers map

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.13';window.__GIT_HASH__='911a5f';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.13';window.__GIT_HASH__='a0c42d';window.__BUILD_DATE__='2026-04-11';
