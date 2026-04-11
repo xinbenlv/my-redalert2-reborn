ce537600aab1efc551cc37a0447cc62163dc4b15
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 16:09:52 -0700

    build: bump version after neutral city garrisons

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ else
     exit 1
 fi
 
-VERSION="0.3.42"
+VERSION="0.3.43"
 DATE=$(date -u +"%Y-%m-%d")
 echo "window.__VERSION__='${VERSION}';window.__GIT_HASH__='${HASH}';window.__BUILD_DATE__='${DATE}';" > js/version.js
 echo "Built version ${VERSION} (${HASH}) on ${DATE}"
--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.42';window.__GIT_HASH__='477fb6';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.43';window.__GIT_HASH__='47cd29';window.__BUILD_DATE__='2026-04-11';
