1d7278e1a3220403edeb028041c2ea8d3b8928e2
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 07:03:50 -0700

    build: bump version after flak track

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.15';window.__GIT_HASH__='0fa961';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.16';window.__GIT_HASH__='9b77a5';window.__BUILD_DATE__='2026-04-11';
