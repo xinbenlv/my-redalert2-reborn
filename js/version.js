82f7fc416ee783710b231576199298975693c111
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 12:36:37 -0700

    build: bump version after harvester retreat

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.29';window.__GIT_HASH__='6270ea';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.30';window.__GIT_HASH__='03f70b';window.__BUILD_DATE__='2026-04-11';
