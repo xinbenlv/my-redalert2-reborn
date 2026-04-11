cf588696505323f9d04f7636c46a4e38552ceae4
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 06:20:03 -0700

    build: bump version after sandbag walls

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.14';window.__GIT_HASH__='9a6b3e';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.15';window.__GIT_HASH__='0fa961';window.__BUILD_DATE__='2026-04-11';
