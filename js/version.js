522c66dc7a65ff12731e3997f5749e2275b2119b
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 16:27:04 -0700

    build: bump version after force-fire command

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.43';window.__GIT_HASH__='47cd29';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.44';window.__GIT_HASH__='5ca5d8';window.__BUILD_DATE__='2026-04-11';
