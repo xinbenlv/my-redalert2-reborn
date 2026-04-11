213f4eedaa06d4a55576b22dfe8ece07eb6e318f
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 14:14:22 -0700

    build: bump version after neutral map garrisons

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.35';window.__GIT_HASH__='4b58e5';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.36';window.__GIT_HASH__='99f221';window.__BUILD_DATE__='2026-04-11';
