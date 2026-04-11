b388679ddec0f6b7939bedf548605194ccba9021
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-10 21:11:37 -0700

    build: bump version after infantry roster

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.3';window.__GIT_HASH__='1b3ef7';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.4';window.__GIT_HASH__='1b6a3b';window.__BUILD_DATE__='2026-04-11';
