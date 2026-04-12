c92af9045f8bc859af77f21cfd7c10a2ad6aff2f
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 17:06:13 -0700

    build: bump version after ai building economy

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.45';window.__GIT_HASH__='026b72';window.__BUILD_DATE__='2026-04-11';
+window.__VERSION__='0.3.46';window.__GIT_HASH__='0e1896';window.__BUILD_DATE__='2026-04-12';
