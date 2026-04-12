34f6f9020d975aed31e6679e6488af3c00c52062
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 17:22:00 -0700

    build: bump version after ai defense layout

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.46';window.__GIT_HASH__='0e1896';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.47';window.__GIT_HASH__='b6dcb2';window.__BUILD_DATE__='2026-04-12';
