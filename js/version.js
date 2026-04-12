050e41f2e8458c8b9f34a637b10fa90d307a032c
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 18:20:05 -0700

    build: bump version after player color selection

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.50';window.__GIT_HASH__='3c1677';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.51';window.__GIT_HASH__='92ff22';window.__BUILD_DATE__='2026-04-12';
