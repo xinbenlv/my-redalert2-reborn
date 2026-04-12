891068df6408b862f196e17142396967121cac3a
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-12 12:45:08 -0700

    build: bump version after GI deploy infantry

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.57';window.__GIT_HASH__='303426';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.58';window.__GIT_HASH__='5e6239';window.__BUILD_DATE__='2026-04-12';
