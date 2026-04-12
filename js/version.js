5fa2e1e5aa461c68513aac6df57bcf6f5d865215
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 17:51:14 -0700

    build: bump version after gem economy pass

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.48';window.__GIT_HASH__='62bd7e';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.49';window.__GIT_HASH__='dea264';window.__BUILD_DATE__='2026-04-12';
