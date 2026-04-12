eed4402e31d1d960e2ae4b7eab6ca055138a0267
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 18:04:25 -0700

    build: bump version after ai tech climb

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.49';window.__GIT_HASH__='dea264';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.50';window.__GIT_HASH__='3c1677';window.__BUILD_DATE__='2026-04-12';
