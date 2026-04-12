ae4c192110f4503100c2edd825e9fb7273cb2e88
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-12 12:20:37 -0700

    build: bump version after civilian towers

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.56';window.__GIT_HASH__='67e7d7';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.57';window.__GIT_HASH__='303426';window.__BUILD_DATE__='2026-04-12';
