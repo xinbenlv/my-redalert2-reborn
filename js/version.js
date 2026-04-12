44965f145fbd0005d304e627e1b032b95586b9e9
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 18:36:55 -0700

    build: bump version after powered defense pass

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.51';window.__GIT_HASH__='92ff22';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.52';window.__GIT_HASH__='0d8ae5';window.__BUILD_DATE__='2026-04-12';
