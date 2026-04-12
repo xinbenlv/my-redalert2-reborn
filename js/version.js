056dc6ae94ba6e285b3cfd5235d53f4b28cf32c7
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 20:13:14 -0700

    build: bump version after kirov airship

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.54';window.__GIT_HASH__='6cbbea';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.55';window.__GIT_HASH__='e0fd2a';window.__BUILD_DATE__='2026-04-12';
