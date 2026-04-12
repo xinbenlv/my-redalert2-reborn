82239ef7b1510a5daffc6c80ff983ca1e916178f
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 17:33:45 -0700

    build: bump version after ai difficulty pass

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.47';window.__GIT_HASH__='b6dcb2';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.48';window.__GIT_HASH__='62bd7e';window.__BUILD_DATE__='2026-04-12';
