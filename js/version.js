14ffaccca1e5856c6a365c81fe2b9bf34c4d4a37
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-12 13:26:12 -0700

    build: bump version after transport helicopter

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.58';window.__GIT_HASH__='5e6239';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.59';window.__GIT_HASH__='8dc483';window.__BUILD_DATE__='2026-04-12';
