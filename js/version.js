04ddd4bef777360a8eed55b9ea1681c6c2d57534
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-11 19:10:54 -0700

    build: bump version after prism tank

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.53';window.__GIT_HASH__='190e62';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.54';window.__GIT_HASH__='6cbbea';window.__BUILD_DATE__='2026-04-12';
