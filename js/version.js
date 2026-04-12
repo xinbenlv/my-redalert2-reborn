e77fe845910b382855ba464a86cbc92fc074f5c0
Zainan Victor Zhou <zzn+pa@zzn.im>
2026-04-12 14:41:51 -0700

    build: bump version after garrison combat profiles

--- js/version.js
+++ js/version.js
@@ -1,1 +1,1 @@
-window.__VERSION__='0.3.61';window.__GIT_HASH__='c49005';window.__BUILD_DATE__='2026-04-12';
+window.__VERSION__='0.3.62';window.__GIT_HASH__='6f7f4c';window.__BUILD_DATE__='2026-04-12';
